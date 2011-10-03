a7fb01f74fc35395d8f5e8222ac99738cf1fa009
alex chousmith <alex@ninthlink.com>
2011-10-03 18:05:25 +0000

    [BugzID:4053] made product (thumbnail) images on http://www.progo.com/themes/ click through to product details pages

--- wpsc-products_page.php
+++ wpsc-products_page.php
@@ -70,8 +70,8 @@ global $wp_query;
 					<div class="imagecol" id="imagecol_<?php echo wpsc_the_product_id(); ?>">
 						<?php if(wpsc_the_product_thumbnail()) :
 						?>
-							<a rel="<?php echo wpsc_the_product_title(); ?>" class="<?php echo wpsc_the_product_image_link_classes(); ?>" href="<?php echo wpsc_the_product_image(); ?>">
-								<img class="product_image" id="product_image_<?php echo wpsc_the_product_id(); ?>" alt="<?php echo wpsc_the_product_title(); ?>" title="<?php echo wpsc_the_product_title(); ?>" src="<?php echo wpsc_the_product_image(); ?>"/>
+							<a href="<?php echo wpsc_the_product_permalink(); ?>">
+								<img id="product_image_<?php echo wpsc_the_product_id(); ?>" alt="<?php echo wpsc_the_product_title(); ?>" title="<?php echo wpsc_the_product_title(); ?>" src="<?php echo wpsc_the_product_image(); ?>"/>
 							</a>
 						<?php else: ?>
 								<a href="<?php echo wpsc_the_product_permalink(); ?>">
